8b753a7809b2668d620fa603877e5c4855da49d7
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-03-04 18:21:39 +0530

    middleware

--- Express/SignUp/signup.js
+++ Express/SignUp/signup.js
@@ -3,16 +3,31 @@ const express = require("express");
 const app = express();
 
 app.listen(3000);
-app.use(express.json());
+app.use(express.json()); //global middleware
 
 const authRouter = express.Router();
 
 app.use("/auth", authRouter);
 
-authRouter.route("/signup").get(getSignUp).post(postSignUp);
+authRouter
+	.route("/signup")
+	.get(middleware1, getSignUp, middleware2) //path specific middleware
+	.post(postSignUp);
 
-function getSignUp(req, res) {
+function middleware1(req, res, next) {
+	console.log("middleware1 encountered");
+	next();
+}
+function middleware2(req, res, next) {
+	console.log("middleware2 encountered");
+	console.log("middleware2 ended req/res cycle");
 	res.sendFile("./index.html", { root: __dirname });
+}
+
+function getSignUp(req, res, next) {
+	console.log("🚀 ~ file: getSignUp called");
+	// res.sendFile("./index.html", { root: __dirname });
+	next();
 }
 
 function postSignUp(req, res) {
